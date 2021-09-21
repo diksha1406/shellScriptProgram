cf235e4c3c3d706d68a8813afb586e96f3e2523a
diksha1221 <dikshagawande0@gmail.com>
2021-09-21 18:12:01 +0530

    Added : new file of case

---
+++ caseNumber.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+echo "enter a number : "
+read n
+case $n in
+1) 
+echo "you enter ONE"
+;;
+2)
+echo "you enter Two"
+;;
+3)
+echo "you enter THREE"
+;;
+4)
+echo "you enter FOUR"
+;;
+5)
+echo "you enter FIVE"
+;;
+6)
+echo "you enter SIX"
+;;
+7)
+echo "you enter SEVEN"
+;;
+8)
+echo "you enter EIGHT"
+;;
+9)
+echo "you enter NINE"
+;;
+*)
+echo "invalid number"
+esac
